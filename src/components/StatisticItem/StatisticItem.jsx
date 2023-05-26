8cbaba38d9566a2bd897356386b4d46ed70270e9
Влад Язиков <vladzaraza@gmail.com>
2023-05-26 21:43:22 +0300

    123

--- src/components/StatisticItem/StatisticItem.jsx
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -4,6 +4,12 @@ import {
   StatisticCounter,
 } from './StatisticItem.styled';
 
-export const StatisticItem = () => {
-  return <div>StatisticBox</div>;
+export const StatisticItem = ({ total, title, icon }) => {
+  return (
+    <StatisticBox>
+      {icon}
+      <StatisticCounter>{total}</StatisticCounter>
+      <StatisticText>{title}</StatisticText>
+    </StatisticBox>
+  );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,10 +4,30 @@ import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+const icons = [
+  <FaRegThumbsUp />,
+  <MdPeople />,
+  <MdOutlineProductionQuantityLimits />,
+  <GiTreeDoor />,
+];
+
+export const Statistics = ({ stats, title }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      {title && <StatisticTitle>{title}</StatisticTitle>}
+
+      <StatisticsList>
+        {stats.map(({ id, title, total }, idx) => {
+          return (
+            <StatisticItem
+              key={id}
+              icon={icons[idx]}
+              title={title}
+              total={total}
+            />
+          );
+        })}
+      </StatisticsList>
     </>
   );
 };
